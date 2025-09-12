3a9b79410402128e4e9787b03ccea82606aeb6f5
shoaibmiaan <151955583+shoaibmiaan@users.noreply.github.com>
2025-09-12 20:33:37 +0000

    fix(ts): use type-only import for InputProps (verbatimModuleSyntax)

--- components/design-system/PasswordInput.tsx
+++ components/design-system/PasswordInput.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
-import { Input, InputProps } from './Input'
+import { Input } from './Input'
+import type { InputProps } from './Input'
 export const PasswordInput: React.FC<InputProps> = (props) => <Input {...props} type="password" passwordToggle />
 export default PasswordInput
