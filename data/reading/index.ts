b3303ee8f1d277ddf75867141ff6e3d00c7caaab
shoaibmiaan <151955583+shoaibmiaan@users.noreply.github.com>
2025-10-28 12:45:29 +0500

    Include all reading tests in practice list

--- data/reading/index.ts
+++ data/reading/index.ts
@@ -26,6 +26,17 @@ import practice03 from './reading-practice-03.json';
 import practice04 from './reading-practice-04.json';
 import practice05 from './reading-practice-05.json';
 import fullExam from './full-exam-001.json';
+import readingTest01 from './reading-test-1.json';
+import readingTest02 from './reading-test-2.json';
+import readingTest03 from './reading-test-3.json';
+import readingTest04 from './reading-test-4.json';
+import readingTest05 from './reading-test-5.json';
+import readingTest06 from './reading-test-6.json';
+import readingTest07 from './reading-test-7.json';
+import readingTest08 from './reading-test-8.json';
+import readingTest09 from './reading-test-9.json';
+import readingTest10 from './reading-test-10.json';
+import readingTest11 from './reading-test-11.json';
 
 const readingPracticeJson = [
   practice01,
@@ -34,6 +45,17 @@ const readingPracticeJson = [
   practice04,
   practice05,
   fullExam,
+  readingTest01,
+  readingTest02,
+  readingTest03,
+  readingTest04,
+  readingTest05,
+  readingTest06,
+  readingTest07,
+  readingTest08,
+  readingTest09,
+  readingTest10,
+  readingTest11,
 ] satisfies ReadingPaper[];
 
 export const readingPracticePapers: ReadingPaper[] = readingPracticeJson;
