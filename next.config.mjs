2ada5d7826b7147a12a784a3140b816ba5c029eb
shoaibmiaan <151955583+shoaibmiaan@users.noreply.github.com>
2026-03-01 15:56:56 +0500

    Exclude app-build-manifest from PWA precache (#34)

--- next.config.mjs
+++ next.config.mjs
@@ -118,7 +118,7 @@ export default withPWA({
   disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
-  buildExcludes: [/.*\.map$/, /middleware-manifest\.json$/, /server\/middleware-manifest\.json$/],
+  buildExcludes: [/.*\.map$/, /middleware-manifest\.json$/, /server\/middleware-manifest\.json$/, /app-build-manifest\.json$/],
   publicExcludes: ['**/*.map'],
   importScripts: ['sw-sync.js', 'sw-patches/push-handler.js'],
 })(baseConfig);
