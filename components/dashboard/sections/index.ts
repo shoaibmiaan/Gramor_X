f75a37b5443bc1163e1b430a5b81e910937d2532
shoaibmiaan <151955583+shoaibmiaan@users.noreply.github.com>
2026-03-03 23:27:37 +0500

    remove unnecessary dashboard section files and simplify page composition

--- components/dashboard/sections/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './IdentityStatusSection';
-export * from './NextActionsSection';
-export * from './AnalyticsSection';
-export * from './MotivationSection';
-export * from './UtilitiesSection';
